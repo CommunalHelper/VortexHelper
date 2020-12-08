9b3ff1fff1123104c2bf4ea817701170047ef376
pixelator <45022852+PIXELATOR37@users.noreply.github.com>
2020-12-07 21:46:15 -0500

    Attached jumpthru Ahorn plugin

---
+++ Ahorn/entities/attachedJumpThru.jl
@@ -0,0 +1,70 @@
+module VortexHelperAttachedJumpThru
+using ..Ahorn, Maple
+
+@mapdef Entity "VortexHelper/AttachedJumpThru" AttachedJumpThru(x::Integer, y::Integer,
+width::Integer=8, texture::String="wood", surfaceIndex::Int=-1)
+
+const textures = ["wood", "dream", "temple", "templeB", "cliffside", "reflection", "core", "moon"]
+const placements = Ahorn.PlacementDict(
+    "Attached Jump Through ($(uppercasefirst(texture))) (Vortex Helper) (IN BETA)" => Ahorn.EntityPlacement(
+        AttachedJumpThru,
+        "rectangle",
+        Dict{String, Any}(
+            "texture" => texture
+        )
+    ) for texture in textures
+)
+
+const quads = Tuple{Integer, Integer, Integer, Integer}[
+    (0, 0, 8, 7) (8, 0, 8, 7) (16, 0, 8, 7);
+    (0, 8, 8, 5) (8, 8, 8, 5) (16, 8, 8, 5)
+]
+
+Ahorn.editingOptions(entity::AttachedJumpThru) = Dict{String, Any}(
+    "texture" => textures,
+    "surfaceIndex" => Maple.tileset_sound_ids
+)
+
+Ahorn.minimumSize(entity::AttachedJumpThru) = 8, 0
+Ahorn.resizable(entity::AttachedJumpThru) = true, false
+
+function Ahorn.selection(entity::AttachedJumpThru)
+    x, y = Ahorn.position(entity)
+    width = Int(get(entity.data, "width", 8))
+
+    return Ahorn.Rectangle(x, y, width, 8)
+end
+
+function Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::AttachedJumpThru, room::Maple.Room)
+    texture = get(entity.data, "texture", "wood")
+    texture = texture == "default" ? "wood" : texture
+
+    # Values need to be system specific integer
+    x = Int(get(entity.data, "x", 0))
+    y = Int(get(entity.data, "y", 0))
+
+    width = Int(get(entity.data, "width", 8))
+
+    startX = div(x, 8) + 1
+    stopX = startX + div(width, 8) - 1
+    startY = div(y, 8) + 1
+
+    len = stopX - startX
+    for i in 0:len
+        connected = false
+        qx = 2
+        if i == 0
+            connected = get(room.fgTiles.data, (startY, startX - 1), false) != '0'
+            qx = 1
+
+        elseif i == len
+            connected = get(room.fgTiles.data, (startY, stopX + 1), false) != '0'
+            qx = 3
+        end
+
+        quad = quads[2 - connected, qx]
+        Ahorn.drawImage(ctx, "objects/jumpthru/$(texture)", 8 * i, 0, quad[1], quad[2], quad[3], quad[4])
+    end
+end
+
+end
